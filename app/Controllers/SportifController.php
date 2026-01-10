9e5ec493b13a89d812353ed58e07121ab48ca12a
ayouberrak <ayouberrak30@gmail.com>
2026-01-10 10:41:18 +0100

    [Controllers] Update SportifController logic

--- app/Controllers/SportifController.php
+++ app/Controllers/SportifController.php
@@ -4,5 +4,10 @@ namespace App\Controllers;
 use Core\Controller;
 
 class SportifController extends Controller {
-    // Sportif management
+    public function index() {
+        $data = [
+            'user_role' => 'sportif'
+        ];
+        $this->render('sportif/dashboard.twig', $data);
+    }
 }
