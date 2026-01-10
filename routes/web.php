e8e3d7c0c5aab41a3f76b11a70e13abecb595811
ayouberrak <ayouberrak30@gmail.com>
2026-01-10 10:41:20 +0100

    [Routes] Update web routes

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,8 @@
 use Core\Router;
 
 // Define routes here
+$router->get('/', [App\Controllers\CoachController::class, 'index']); // Temporary: make home the coach dashboard for ease of access
+$router->get('/coach/dashboard', [App\Controllers\CoachController::class, 'index']);
+$router->get('/sportif/dashboard', [App\Controllers\SportifController::class, 'index']);
+$router->get('/login', [App\Controllers\AuthController::class, 'login']);
+
