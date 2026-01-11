4623b4277742fcb05077ecb1f3b09da6df8e6591
ayouberrak <ayouberrak30@gmail.com>
2026-01-11 23:18:32 +0100

    [Public] Update application entry point

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ .'/../core/helpers.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Core\Router;
-
+define('BASE_URL', '/coachPro_v3');
 $router = new Router();
 
 // Load Routes
