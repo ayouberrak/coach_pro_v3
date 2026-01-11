5f71b35b3b7554477add9a659a9ed8fb60d1fee9
ayouberrak <ayouberrak30@gmail.com>
2026-01-11 23:18:30 +0100

    [Models] Update Sportif model

--- app/Models/Sportif.php
+++ app/Models/Sportif.php
@@ -3,11 +3,11 @@ namespace App\Models;
 
 class Sportif extends User {
 
-    private int $id_sportif;
-    private int $numero_telephone;
+    private ?int $id_sportif;
+    private string $numero_telephone;
 
-    public function __construct(int $id, string $firstName, string $lastName, string $email, string $passwordHash, int $role,
-                                int $id_sportif, int $numero_telephone) {
+    public function __construct(?int $id = null, string $firstName = "", string $lastName = "", string $email = "", string $passwordHash = "", int $role = 1,
+                                ?int $id_sportif = null, string $numero_telephone) {
         parent::__construct($id, $firstName, $lastName, $email, $passwordHash, $role);
         $this->id_sportif = $id_sportif;
         $this->numero_telephone = $numero_telephone;
@@ -15,11 +15,23 @@ class Sportif extends User {
     public function getIdSportif(): int {
         return $this->id_sportif;
     }
-    public function getNumeroTelephone(): int {
+    public function getNumeroTelephone(): string {
         return $this->numero_telephone;
     }
-    public function setNumeroTelephone(int $numero_telephone): void {
+    public function setNumeroTelephone(string $numero_telephone): void {
         $this->numero_telephone = $numero_telephone;
     }
 
+    public static function createFromArray(array $data): Sportif {
+        return new Sportif(
+            $data['id'] ?? null,
+            $data['firstName'] ?? '',
+            $data['lastName'] ?? '',
+            $data['email'] ?? '',
+            $data['passwordHash'] ?? '',
+            $data['role'] ?? 1,
+            $data['id_sportif'] ?? null,
+            $data['numero_telephone'] ?? ''
+        );
+    }
 }
