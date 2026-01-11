d455808a7d4a1d15a9bea4b219f3910e8d3d549b
ayouberrak <ayouberrak30@gmail.com>
2026-01-11 23:18:31 +0100

    [Services] Update AuthServices

--- app/Services/AuthServices.php
+++ app/Services/AuthServices.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Services;
+namespace App\Services;
 use App\Models\User;
-use Repository\UserRepository;
-use Repository\CoachRepository;
-use Repository\SportifRepository;
+use App\Models\Sportif;
+use App\Repository\UserRepository;
+use App\Repository\CoachRepository;
+use App\Repository\SportifRepository;
 use Exception;
 
 class AuthServices {
@@ -12,24 +13,46 @@ class AuthServices {
     private UserRepository $userRepository;
     private CoachRepository $coachRepository; 
     private SportifRepository $sportifRepository;
-
+ 
     public function __construct() {
         $this->userRepository = new UserRepository();
         $this->coachRepository = new CoachRepository();
         $this->sportifRepository = new SportifRepository();
     }
 
-    public function registerUser(User $user): bool {
+    public function registerUser(array $data , ?array $coachData = null, ?array $sportifData = null): bool {
         try {
+            $user = new User(
+                null,
+                $data['nom'],
+                $data['prenom'],
+                $data['email'],
+                password_hash($data['password'], PASSWORD_BCRYPT),
+                (int)$data['role']
+            );
+
             $this->userRepository->createUser($user);
-            if ($user->getRole() === 2) { 
-                return $this->coachRepository->createCoach($user);
-            } elseif ($user->getRole() === 3) { 
-                return $this->sportifRepository->createSportif($user);
+
+            if ($data['role'] == 2 && $coachData !== null) { 
+                $coach = new \App\Models\Coach(
+                    null,
+                    $coachData['biographie'],
+                    $coachData['photo'],
+                    (int)$coachData['annees_experience'],
+                    $coachData['certification']
+                );
+                return $this->coachRepository->createCoach($coach);
+            } elseif ($data['role'] == 1 && $sportifData !== null) { 
+                $formData = array_merge($data, $sportifData);
+                $sportif = Sportif::createFromArray($formData);
+
+                return $this->sportifRepository->createSportif($sportif);
             }
+
             return true;
         } catch (Exception $e) {
             throw new Exception("Registration failed: " . $e->getMessage());
         }
+
     }
 }
