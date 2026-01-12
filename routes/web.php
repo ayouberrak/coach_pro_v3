18aa45468ce6f8ca4929d95317fdd440422fe457
ayouberrak <ayouberrak30@gmail.com>
2026-01-12 11:43:05 +0100

    [Routes] Update web routes

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,18 @@ use Core\Router;
 
 // Define routes here
 // $router->get('/a/config', [Config\Database::class, 'showConfig']);
+$router->get('/', [App\Controllers\CoachController::class, 'index']);
 
 
 $router->get('/register', [App\Controllers\AuthController::class, 'showRegisterForm']);
 $router->post('/register', [App\Controllers\AuthController::class, 'register']);
 
+$router->post('/login', [App\Controllers\AuthController::class, 'Login']);
+
+$router->get('/logout', [App\Controllers\AuthController::class, 'logout']);
 
 $router->get('/coach/profile', [App\Controllers\ProfileController::class, 'showProfile']);
-$router->get('/', [App\Controllers\CoachController::class, 'index']);
+
 $router->get('/coach/dashboard', [App\Controllers\CoachController::class, 'index']);
 $router->get('/sportif/dashboard', [App\Controllers\SportifController::class, 'index']);
 $router->get('/login', [App\Controllers\AuthController::class, 'login']);
