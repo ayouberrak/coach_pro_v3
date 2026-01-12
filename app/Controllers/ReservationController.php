dea7adf11f2659d05a20af4eba861e876487797b
ayouberrak <ayouberrak30@gmail.com>
2026-01-12 15:09:30 +0100

    [Controllers] Update ReservationController

--- app/Controllers/ReservationController.php
+++ app/Controllers/ReservationController.php
@@ -4,5 +4,11 @@ namespace App\Controllers;
 use Core\Controller;
 
 class ReservationController extends Controller {
-    // Reservation management
+    public function index() {
+        session_start();
+        \App\Middleware\AuthMiddleware::handleCoach();
+         $user_role = $_SESSION['user_role'] ;
+
+        $this->render('coach/reservations.twig', ['user_role' => $user_role]);
+    }
 }
