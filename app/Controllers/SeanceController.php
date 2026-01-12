9a3e761e0b8b8d700c91d6e9be45187ba3937a94
ayouberrak <ayouberrak30@gmail.com>
2026-01-12 15:09:30 +0100

    [Controllers] Update SeanceController

--- app/Controllers/SeanceController.php
+++ app/Controllers/SeanceController.php
@@ -1,8 +1,14 @@
 <?php
 namespace App\Controllers;
 
+use \App\Middleware\AuthMiddleware;
 use Core\Controller;
 
 class SeanceController extends Controller {
-    // Session CRUD
+    public function index() {
+        session_start();
+        $user_role = $_SESSION['user_role'] ;
+        AuthMiddleware::handleSportif();
+        $this->render('sportif/seances.twig', ['user_role' => $user_role]);
+    }
 }
