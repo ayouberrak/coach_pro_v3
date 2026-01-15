4e0623eb79d1639fb0e7f214adba940874284e5b
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 15:02:19 +0100

    fix(repository): update ReservationRepository

--- app/Repository/ReservationRepository.php
+++ app/Repository/ReservationRepository.php
@@ -32,9 +32,10 @@ class ReservationRepository {
 
     public function getReservationsEnAttenteByIdClient(int $idClient): array {
         try {
-            $stmt = $this->db->prepare("SELECT * FROM seances s
+            $stmt = $this->db->prepare("SELECT *   FROM seances s
                                                 inner join users u on u.id = s.id_coach
                                                 inner join coach c on c.id_coach = u.id
+                                                inner join status st on st.id_status = s.id_status
                                                 WHERE id_client = :id_client
                                                 AND s.id_status != 4 ");
 
@@ -55,7 +56,8 @@ class ReservationRepository {
                         $data['id_status']
                     ),
                     'nomCoach' => $data['prenom'] . ' ' . $data['nom'],
-                    'coachPhoto' => $data['photo']
+                    'coachPhoto' =>  '/coachPro_v3/public/uploads/'.$data['photo'],
+                    'status' => $data['type_status'],
                 ];
             }
             return $info;
@@ -64,5 +66,65 @@ class ReservationRepository {
         }
     }
 
+    public function UpdateStatusOnTerminer(){
+        try {
+            $stmt = $this->db->prepare("UPDATE seances 
+                                        SET id_status = 4 
+                                        WHERE (date_seance + debut + (duree || ' minutes')::interval) < NOW()
+                                        AND id_status != 4");
+            return $stmt->execute();
+        } catch (Exception $e) {
+            throw new Exception("Erreur lors de la mise à jour des statuts des réservations: " . $e->getMessage());
+        }   
+    }
 
+    public function getReservationTerminerByClient(int $idClient): array {
+        try {
+            $stmt = $this->db->prepare("SELECT *   FROM seances s
+                                                inner join users u on u.id = s.id_coach
+                                                inner join coach c on c.id_coach = u.id
+                                                inner join status st on st.id_status = s.id_status
+                                                WHERE id_client = :id_client
+                                                AND s.id_status = 4 ");
+
+            $stmt->bindValue(':id_client', $idClient, PDO::PARAM_INT);
+            $stmt->execute();
+            $reservationsData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $info = [];
+            foreach ($reservationsData as $data) {
+                $info[] = [
+                    'reservation' => new Reservation(
+                        $data['id_seance'],
+                        $data['id_client'],
+                        $data['id_coach'],
+                        $data['date_seance'],
+                        $data['debut'],
+                        $data['duree'],
+                        $data['id_status']
+                    ),
+                    'nomCoach' => $data['prenom'] . ' ' . $data['nom'],
+                    'status' => $data['type_status'],
+                ];
+            }
+            return $info;
+        } catch (Exception $e) {
+            throw new Exception("Erreur lors de la récupération des réservations: " . $e->getMessage());
+        }
+
+    }
+
+    public function countResByClient(int $idClient): int {
+        try {
+            $stmt = $this->db->prepare("SELECT COUNT(*) as total FROM seances WHERE id_client = :id_client");
+            $stmt->bindValue(':id_client', $idClient, PDO::PARAM_INT);
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            return (int)$result['total'];
+        } catch (Exception $e) {
+            throw new Exception("Erreur lors du comptage des réservations: " . $e->getMessage());
+        }
+    }
+
+    
 }
