359f693520af231e441b7367b7b85a7669fea4da
ayouberrak <ayouberrak30@gmail.com>
2026-01-15 15:02:18 +0100

    fix(controller): update ProfileSportifController

--- app/Controllers/ProfileSportifController.php
+++ app/Controllers/ProfileSportifController.php
@@ -3,13 +3,20 @@
 namespace App\Controllers;
 use Core\Controller;
 use \App\Middleware\AuthMiddleware;
+use App\Services\SportifServices;
 
 class ProfileSportifController extends Controller {
     public function showProfile() {
         session_start();
         $user_role = $_SESSION['user_role'] ;
 
+        $sportifService = new SportifServices();
+        $sportif = $sportifService->getInfoClientById($_SESSION['user_id']);
+
         AuthMiddleware::handleSportif();
-        $this->render('sportif/profile.twig', ['user_role' => $user_role]);
+        $this->render('sportif/profile.twig', [
+            'user_role' => $user_role ,
+            'sportif' => $sportif
+            ]);
     }
 }
