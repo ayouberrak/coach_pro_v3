326a740ec22f4665eb316ca1cdfd464b41bf8ed2
ayouberrak <ayouberrak30@gmail.com>
2026-01-16 11:32:21 +0100

    fix(repository): update SportifRepository

--- app/Repository/SportifRepository.php
+++ app/Repository/SportifRepository.php
@@ -36,8 +36,45 @@ class SportifRepository {
         $stmt->bindParam(':id_client', $id_client, PDO::PARAM_INT);
         $stmt->execute();
         $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        // print_r($data);
         if ($data) {
-            return Sportif::createFromArray($data);
+            return new Sportif(
+                $data['id'],
+                $data['nom'],
+                $data['prenom'],
+                $data['email'],
+                $data['password'],
+                $data['id_role'],
+                $data['id_client'],
+                $data['telephone']
+            );
         }
+    }
+    public function UpdateSportif(Sportif $sportif , $id): bool {
+        try {
+            $this->db->beginTransaction();
+            $stmt = $this->db->prepare("UPDATE users u SET nom = :nom, prenom = :prenom, email = :email, password = :password
+                                            WHERE u.id = :id;");
+                                            
+            $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+            $stmt->bindValue(':nom', $sportif->getFirstName());
+            $stmt->bindValue(':prenom', $sportif->getLastName());
+            $stmt->bindValue(':email', $sportif->getEmail());
+            $stmt->bindValue(':password', $sportif->getPasswordHash());
+             $stmt->execute();
+
+            $stmt2 = $this->db->prepare("UPDATE client SET telephone = :telephone WHERE id_client = :id_client");
+            $stmt2->bindValue(':id_client', $id, PDO::PARAM_INT);
+            $stmt2->bindValue(':telephone', $sportif->getNumeroTelephone());
+            $stmt2->execute();
+
+            $this->db->commit();
+            return true;
+
+        } catch (PDOException $e) {
+            $this->db->rollBack();
+            throw new \Exception("Erreur lors de la mise à jour du sportif: " . $e->getMessage());
+        }
+
     }
 }
