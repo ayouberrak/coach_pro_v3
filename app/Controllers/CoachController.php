71c74dba039073ff62729f0f80fb764faa34d169
ayouberrak <ayouberrak30@gmail.com>
2026-01-16 11:32:20 +0100

    fix(controller): update CoachController

--- app/Controllers/CoachController.php
+++ app/Controllers/CoachController.php
@@ -3,14 +3,23 @@ namespace App\Controllers;
 
 use Core\Controller;
 use \App\Middleware\AuthMiddleware;
+use App\Services\ReservationServices;
 
 class CoachController extends Controller {
-    public function index() {
+    public function dashboard() {
         session_start();
         $user_role = $_SESSION['user_role'] ;
         AuthMiddleware::handleCoach();
 
-        $this->render('coach/dashboard.twig' , ['user_role' => $user_role]);
+        $resServices = new ReservationServices();
+        $reservations = $resServices->getReservationEnattenteByIdCoach($_SESSION['user_id']);
+
+        // print_r($reservations);
+        $this->render('coach/dashboard.twig' , ['user_role' => $user_role, 'reservations' => $reservations]);
     }
+
+    
+
+
 
 }
