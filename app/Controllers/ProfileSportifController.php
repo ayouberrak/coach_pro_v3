794de05629ea3c3226543a51576fd1ff9b2314de
ayouberrak <ayouberrak30@gmail.com>
2026-01-16 11:32:20 +0100

    fix(controller): update ProfileSportifController

--- app/Controllers/ProfileSportifController.php
+++ app/Controllers/ProfileSportifController.php
@@ -13,10 +13,26 @@ class ProfileSportifController extends Controller {
         $sportifService = new SportifServices();
         $sportif = $sportifService->getInfoClientById($_SESSION['user_id']);
 
+        // print_r($sportif);
+
         AuthMiddleware::handleSportif();
         $this->render('sportif/profile.twig', [
             'user_role' => $user_role ,
             'sportif' => $sportif
             ]);
+    }
+
+    public function editProfile($data) {
+        session_start();
+        $user_role = $_SESSION['user_role'] ;
+
+        if($_SERVER['REQUEST_METHOD'] === "POST") {
+            $sportifService = new SportifServices();
+            $sportifService->updateClientInfo($data, $_SESSION['user_id']);
+            header("Location: ".BASE_URL."/sportif/profile");
+            exit();
+        }
+
+        AuthMiddleware::handleSportif();
     }
 }
