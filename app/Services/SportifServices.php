6bd38c8d734589d1d6a806cb883bd655d13a167c
ayouberrak <ayouberrak30@gmail.com>
2026-01-16 11:32:21 +0100

    fix(service): update SportifServices

--- app/Services/SportifServices.php
+++ app/Services/SportifServices.php
@@ -4,6 +4,7 @@
 namespace App\Services;
 
 use App\Repository\SportifRepository;
+use App\Models\Sportif;
 use Exception;
 
 
@@ -20,6 +21,14 @@ class SportifServices {
             return $this->sportifRepository->getInfoClientById($id_client);
         } catch (Exception $e) {
             throw new Exception("Failed to retrieve client info: " . $e->getMessage());
+        }
+    }
+
+    public function updateClientInfo(array $data , int $id): bool {
+        try {
+            return $this->sportifRepository->UpdateSportif(Sportif::createFromArray($data), $id);
+        } catch (Exception $e) {
+            throw new Exception("Failed to update client info: " . $e->getMessage());
         }
     }
 
