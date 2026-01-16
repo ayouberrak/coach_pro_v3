f893f56b5c394380ad620f25683b01bd6a8fa847
ayouberrak <ayouberrak30@gmail.com>
2026-01-16 16:06:20 +0100

    Update DispoController logic

--- app/Controllers/DispoController.php
+++ app/Controllers/DispoController.php
@@ -2,12 +2,30 @@
 
 namespace App\Controllers;
 use Core\Controller;
+use App\services\DispoServices;
+use App\Middleware\AuthMiddleware;
 
 class DispoController extends Controller {
     public function index() {
         session_start();
-        \App\Middleware\AuthMiddleware::handleCoach();
+        AuthMiddleware::handleCoach();
          $user_role = $_SESSION['user_role'] ;
+
+         $dispoService = new DispoServices();
+         $disponibilities = $dispoService->getAllDispo($_SESSION['user_id']);
+
+        $this->render('coach/disponibilities.twig', [
+                 'user_role' => $user_role,
+                 'disponibilities' => $disponibilities
+            ]);
+    }
+
+    public function create($data) {
+        session_start();
+        AuthMiddleware::handleCoach();
+         $user_role = $_SESSION['user_role'] ;
+         $dispoService = new DispoServices();
+            $dispoService->createDispo($data , $_SESSION['user_id']);
 
         $this->render('coach/disponibilities.twig', ['user_role' => $user_role]);
     }
